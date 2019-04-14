a234d36097c835ffdd329697027758a252ea3e48
shaylavi <shay@esriaustralia.com.au>
2019-04-14 15:51:50 +1000

    working and finalized form sample with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,21 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 })
 export class AppComponent {
   users: AngularFireList<User>;
-  constructor(dbUsers: AngularFireDatabase) {
+  constructor(dbUsers: AngularFireDatabase
+) {
     this.users = dbUsers
       .list<User>('/users');
-      // .valueChanges();
-      // .subscribe(console.log);
-    // const users = dbUsers
-    //   .list<User>('user')
-    //   .valueChanges()
-    //   .subscribe(console.log);
-  }
+    }
 
-  title = 'app';
-  // constructor(
-  //   public id: number,
-  //   public name: string,
-  //   public power: string,
-  //   public alterEgo?: string
-  // ) {}
-  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
-
-  model = new User(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  model = new User('18', 'Dr IQ', 'Chuck Overstreet');
 
   submitted = false;
 
   AddUser() {
-    this.users.push(this.model);
     // this.users.update('user', this.model);
   }
-  // onSubmit() {
-  //   this.submitted = true;
-  // }
-
-  // TODO: Remove this when we're done
-  get diagnostic() {
-    return JSON.stringify(this.model);
+  onSubmit() {
+    this.submitted = true;
+    this.users.push(this.model);
   }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,8 +1,7 @@
 export class User {
   constructor(
-    public id: number,
     public name: string,
-    public power: string,
-    public alterEgo?: string
+    public activity: string,
+    public desires?: string
   ) {}
 }
