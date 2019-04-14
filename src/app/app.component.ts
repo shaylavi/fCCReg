ce54846fc1bb56d535be1f60fa114a495daaf56d
shaylavi <shay@esriaustralia.com.au>
2019-04-14 14:37:15 +1000

    form ready and working - connected to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users: AngularFireList<User>;
+  constructor(dbUsers: AngularFireDatabase) {
+    this.users = dbUsers
+      .list<User>('/users');
+      // .valueChanges();
+      // .subscribe(console.log);
+    // const users = dbUsers
+    //   .list<User>('user')
+    //   .valueChanges()
+    //   .subscribe(console.log);
+  }
+
   title = 'app';
+  // constructor(
+  //   public id: number,
+  //   public name: string,
+  //   public power: string,
+  //   public alterEgo?: string
+  // ) {}
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+  model = new User(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  AddUser() {
+    this.users.push(this.model);
+    // this.users.update('user', this.model);
+  }
+  // onSubmit() {
+  //   this.submitted = true;
+  // }
+
+  // TODO: Remove this when we're done
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase, 'freecodecampmelb')
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
